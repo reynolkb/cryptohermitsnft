eda9ea0aff0742d458dcef33e2e205797c106749
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-11-14 12:10:59 -0400

    updated scrolling for minter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/Navbar/Navbar';
+import ScrollToTop from './util/ScrollToTop';
 import Home from './components/Home/Home';
 import Minter from './components/Minter/Minter';
 import Footer from './components/Footer/Footer';
@@ -12,6 +13,7 @@ class App extends Component {
 			<BrowserRouter>
 				<div className='App'>
 					<NavBar />
+					<ScrollToTop />
 					<Routes>
 						<Route path='/' element={<Home />} />
 						<Route path='/mint' element={<Minter />} />
