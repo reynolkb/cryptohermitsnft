4a6d9866759a91a76f19d3514d71efd3368e1702
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-12-01 18:10:27 -0400

    updated home page cover photo

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -8,17 +8,20 @@ import './home.css';
 export default function Home(props) {
 	const navigate = useNavigate();
 	return (
-		<div className='page-home center-image'>
-			<img src={coverPhotoTablet} className='cover-photo-tablet' alt='coverPhoto' />
-			<img src={coverPhotoMobile} className='cover-photo-mobile' alt='coverPhoto' />
-			<p className='text-magento-noborder mobile-header-text'>Crypto</p>
-			<p className='text-cyan mobile-header-text'>Hermits</p>
-			<p className='text-normal-black para-home'>
-				Join the revolution of bookworms, self sufficient homesteaders, homeschoolers, fighters of free speech and leaders of ethical sustainable living.
-			</p>
-			<button className='btn-black' style={{ marginTop: 40 }} onClick={() => navigate('/about')}>
-				ENTER
-			</button>
+		<div className='page-home'>
+			<div className='cover-photo-container'>
+				<img src={coverPhotoTablet} className='cover-photo-tablet' alt='coverPhoto' />
+				<img src={coverPhotoMobile} className='cover-photo-mobile' alt='coverPhoto' />
+				<div className='centered'>
+					<p className='text-magento-noborder mobile-header-text'>Crypto</p>
+					<p className='text-cyan mobile-header-text'>Hermits</p>
+					{/* <p className='text-normal-black para-home'> */}
+					<p className='p-home'>Join the revolution of bookworms, self sufficient homesteaders, homeschoolers, fighters of free speech and leaders of ethical sustainable living.</p>
+					<button className='btn-black' style={{ marginTop: 40 }} onClick={() => navigate('/about')}>
+						ENTER
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
