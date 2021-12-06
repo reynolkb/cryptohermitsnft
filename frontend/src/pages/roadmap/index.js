a2aa44a2eab51f5f7c6c6591d28ec085e1a10e9e
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-12-06 12:52:50 -0500

    updated grammar

--- frontend/src/pages/roadmap/index.js
+++ frontend/src/pages/roadmap/index.js
@@ -8,8 +8,8 @@ export default function RoadMap(props) {
 			<p className='text-magento-border'>Roadmap</p>
 			{/* <p className="text-normal-black" style={{ maxWidth: 542, marginTop: 35 }}> */}
 			<p className='text-normal-black para-roadmap' style={{ maxWidth: 542, marginTop: 35 }}>
-				We are starting off with the bookworm collection and if we reach 100% we will begin working on the homesteader collection. All bookworm holders have presale access to the homesteader
-				collection.
+				We are starting off with the bookworm collection and if we reach 100% we will begin working on the homesteader collection. All bookworm holders will have presale access to the
+				homesteader collection.
 			</p>
 			<div className='roadmap-step'>
 				<p className='roadmap-percent'>25%</p>
@@ -21,7 +21,7 @@ export default function RoadMap(props) {
 				<p className='roadmap-percent'>50%</p>
 				<ul>
 					<li className='space-list'>All-inclusive remote tropical vacation for one random wallet address</li>
-					<li className='space-list'>Airdrop Mythic one of one NFT</li>
+					<li className='space-list'>Airdrop Mythic NFT</li>
 				</ul>
 			</div>
 			<div className='roadmap-step'>
@@ -35,7 +35,7 @@ export default function RoadMap(props) {
 				<p className='roadmap-percent'>100%</p>
 				<ul>
 					<li className='space-list'>All-inclusive remote tropical vacation for one random wallet address</li>
-					<li className='space-list'>Airdrop Mythic one of one NFT</li>
+					<li className='space-list'>Airdrop Mythic NFT</li>
 				</ul>
 			</div>
 		</div>
