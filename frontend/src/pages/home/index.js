44d7e5ed01266293f7a17fd24e1e1ccd1c90457a
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-12-21 15:16:58 -0800

    updated website copy:

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -15,9 +15,7 @@ export default function Home(props) {
 			{/* <p className='text-normal-black para-home'> */}
 			<p className='text-normal-black p-home'>
 				The Bookworms are a collection of 5,555 NFTs sitting on top of the Ethereum blockchain costing 0.05 ETH each. After decades of down time and solitude, they are ready to make some
-				noise. Free thinking has no political party. We heed to grandma's telltale wisdom of never judging a book by its cover. We are modern day rebels who are overlooked and without credit.
-				Join the CryptoHermit revolution of bookworms, self-sufficient homesteaders, fighters of free speech and leaders of ethical, sustainable living. Collect art and make friends with those
-				who are just as intentional (and probably fed up) as you. We just want to be left alone and leave others alone. Let's find peace in solitary, together.
+				noise. Free thinking has no political party. We heed to grandma's telltale wisdom of never judging a book by its cover.
 			</p>
 			<button className='btn-black' style={{ marginTop: 40 }} onClick={() => navigate('/rarity')}>
 				LEARN MORE
